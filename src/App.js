40421975d4246e78d13188ac8a64a1b4b6231f51
Elgun <elgunnb@code.edu.az>
2023-02-24 21:15:13 +0400

    homepage-design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import './assets/style/main.css'
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-    Hello World
+    <Home/>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import header from '../assets/style/header.module.css'
+import logo from '../assets/images/logo/Logo_of_Unsplash.svg'
+import SearchIcon from '@mui/icons-material/Search';
+import Profile from './Profile';
+
 
 function Header() {
-  return (
-    <div>
-      
+  return (<>
+  <div  className={header.container}>
+    <div className={header.logo}><img src={logo}/></div>
+   
+    <div className={header.input}><button className={header.search_button}><SearchIcon color="action" fontSize='medium'/></button><input className={header.search_input} placeholder='Search'/></div>
+    <button className={header.login}>Login</button>
+    
     </div>
+    <Profile/>
+  </>
+    
   )
 }
 
