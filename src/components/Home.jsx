88df187334d132ef56fbff6a86e585b3492771c8
Elgun <elgunnb@code.edu.az>
2023-02-27 00:32:17 +0400

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,82 @@
 import React from 'react'
 import Header from './Header'
+import { ImageList, ImageListItem } from "@mui/material";
+import homeCSS from "../assets/style/home.module.css";
+import photo1 from "../assets/images/gallery/photo-1.jpg";
+import photo2 from "../assets/images/gallery/photo-2.jpg";
+import photo3 from "../assets/images/gallery/photo-3.jpg";
+import photo4 from "../assets/images/gallery/photo-4.jpg";
+import photo5 from "../assets/images/gallery/photo-5.jpg";
+import photo6 from "../assets/images/gallery/photo-6.jpg";
+import photo7 from "../assets/images/gallery/photo-7.jpg";
+import photo8 from "../assets/images/gallery/photo-8.jpg";
+import photo9 from "../assets/images/gallery/photo-9.jpg";
 
 
 function Home() {
+
+  const itemData = [
+    {
+      img: photo1,
+      title: "salam",
+    },
+    {
+      img: photo2,
+      title: "salam",
+    },
+    {
+      img: photo3,
+      title: "salam",
+    },
+    {
+      img: photo4,
+      title: "salam",
+    },
+    {
+      img: photo5,
+      title: "salam",
+    },
+    {
+      img: photo6,
+      title: "salam",
+    },
+    {
+      img: photo7,
+      title: "salam",
+    },
+    {
+      img: photo8,
+      title: "salam",
+    },
+    {
+      img: photo9,
+      title: "salam",
+    },
+  ];
   return (
-    <div>
-      <Header/>
+    <>
+<Header/>
+<div className={homeCSS.container}>
+        <div className={homeCSS.card}>
+        <ImageList variant="masonry" cols={3} gap={12}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+            //   loading="lazy"
+            />
+            <div className={homeCSS.absolute}><p>Salam</p></div>
+          </ImageListItem>
+        ))}
+      </ImageList>
+        </div>
+      
     </div>
+      
+    </>
+
   )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,74 +1,11 @@
 import React from "react";
-import { ImageList, ImageListItem } from "@mui/material";
-import profileCSS from "../assets/style/profile.module.css";
-import photo1 from "../assets/images/gallery/photo-1.jpg";
-import photo2 from "../assets/images/gallery/photo-2.jpg";
-import photo3 from "../assets/images/gallery/photo-3.jpg";
-import photo4 from "../assets/images/gallery/photo-4.jpg";
-import photo5 from "../assets/images/gallery/photo-5.jpg";
-import photo6 from "../assets/images/gallery/photo-6.jpg";
-import photo7 from "../assets/images/gallery/photo-7.jpg";
-import photo8 from "../assets/images/gallery/photo-8.jpg";
-import photo9 from "../assets/images/gallery/photo-9.jpg";
+
 
 function Profile() {
-  const itemData = [
-    {
-      img: photo1,
-      title: "salam",
-    },
-    {
-      img: photo2,
-      title: "salam",
-    },
-    {
-      img: photo3,
-      title: "salam",
-    },
-    {
-      img: photo4,
-      title: "salam",
-    },
-    {
-      img: photo5,
-      title: "salam",
-    },
-    {
-      img: photo6,
-      title: "salam",
-    },
-    {
-      img: photo7,
-      title: "salam",
-    },
-    {
-      img: photo8,
-      title: "salam",
-    },
-    {
-      img: photo9,
-      title: "salam",
-    },
-  ];
+  
   return (
-    <div className={profileCSS.container}>
-        <div className={profileCSS.salam}>
-        <ImageList variant="masonry" cols={3} gap={12}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-            //   loading="lazy"
-            />
-            <div className={profileCSS.absolute}><p>Salam</p></div>
-          </ImageListItem>
-        ))}
-      </ImageList>
-        </div>
-      
-    </div>
+    <>
+    </>
   );
 }
 
